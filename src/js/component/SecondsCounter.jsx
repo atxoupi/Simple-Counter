db360cd96ad852f3f94143dbc0ef5e1a8adfac6f
atxoupi <andres.hermelo@gmail.com>
2022-06-07 19:16:41 +0000

    Funcionamiento básico conseguido

--- src/js/component/SecondsCounter.jsx
+++ src/js/component/SecondsCounter.jsx
@@ -10,27 +10,32 @@ const SecondsCounter = ({ segundos, s2, s3, s4 }) => {
 					Mi Contador:
 				</h1>
 			</div>
-			<div className="d-flex justify-content-center p-2 m-auto shadow-lg p-3 mb-5 bg-body rounded w-25">
+			<div className="d-flex justify-content-center p-2 m-auto shadow-lg p-3 mb-5 bg-body rounded w-25 h-25">
 				<div
-					className="bg-secondary fs-1 text shadow-lg p-3 mb-5 rounded text-center"
+					className="bg-secondary fs-1 text shadow-lg p-3 m-auto mb-5 rounded text-center "
+					style={{ width: "70px" }}>
+					<i className="fa-solid fa-clock"></i>
+				</div>
+				<div
+					className="bg-secondary fs-1 text shadow-lg p-3 m-2 mb-5 rounded text-center"
 					style={{ width: "40px" }}>
 					{s4}
 				</div>
-				<div className="fs-1 text M-2 "> : </div>
+
 				<div
-					className="bg-secondary fs-1 text shadow-lg p-3 mb-5 rounded text-center"
+					className="bg-secondary fs-1 text shadow-lg p-3 m-2 mb-5 rounded text-center"
 					style={{ width: "40px" }}>
 					{s3}
 				</div>
-				<div className="fs-1 text M-2 "> : </div>
+
 				<div
-					className="bg-secondary fs-1 text shadow-lg p-3 mb-5 rounded text-center"
+					className="bg-secondary fs-1 text shadow-lg p-3 m-2 mb-5 rounded text-center"
 					style={{ width: "40px" }}>
 					{s2}
 				</div>
-				<div className="fs-1 text M-2 "> : </div>
+
 				<div
-					className="bg-secondary fs-1 text shadow-lg p-3 mb-5 rounded text-center"
+					className="bg-secondary fs-1 text shadow-lg p-3 m-2 mb-5 rounded text-center"
 					style={{ width: "40px" }}>
 					{segundos}
 				</div>
